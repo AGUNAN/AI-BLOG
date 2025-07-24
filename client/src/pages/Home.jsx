4add9058b9a557eeccd11a6984ec62bb8dcd297f
AGUNAN <agunan11@gmail.com>
2025-07-24 20:24:41 +0530

    newsletter.jsx created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Header from '../components/Header';
 import BlogList from '../components/BlogList';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <NavBar />
       <Header />
       <BlogList />
+      <Newsletter />
     </div>
   );
 };
