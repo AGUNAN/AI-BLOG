e998c3acb9158e3ba54a93b314ab723641096fb4
AGUNAN <agunan11@gmail.com>
2025-07-24 21:35:19 +0530

    blog.jsx in process and video at 1:7:15

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { assets, blog_data } from '../asset/assets.js';
+import NavBar from '../components/NavBar.jsx';
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const { id } = useParams(); //to get id from link
+
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = async () => {
+    const data = await blog_data.find((items) => items._id === id);
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+  return data ? (
+    <div className="relative">
+      <img
+        src={assets.gradientBackground}
+        alt=""
+        className="absolute -top-50 -z-1 opacity-50"
+      />
+      <NavBar />
+      <div>
+        <p>Published on {data.createdAt}</p>
+      </div>
+      <div></div>
+    </div>
+  ) : (
+    <div>
+      <h1>loading</h1>
+    </div>
+  );
 };
 
 export default Blog;
